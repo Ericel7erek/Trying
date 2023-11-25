603213347547002ec83b33e880e52eb7c5c8af0b
Eric <ericmalah@Erics-MacBook-Pro.local>
2023-11-25 12:45:01 +0100

    save

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-
+//s
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
