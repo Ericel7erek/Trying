23cd3f7493f80109df64fd02d16b0e9fbe3e1017
Eric <ericmalah@Erics-MacBook-Pro.local>
2023-11-25 12:39:27 +0100

    A

--- public/script.js
+++ public/script.js
@@ -1,19 +1,26 @@
+// Connect to the server
+const socket = io();
+
+// Listen for incoming messages from the server
+socket.on('message', (message) => {
+  const chatWindow = document.getElementById('chat-window');
+  const newMessage = document.createElement('p');
+  newMessage.textContent = message;
+  chatWindow.appendChild(newMessage);
+
+  // Scroll to the bottom to show the latest message
+  chatWindow.scrollTop = chatWindow.scrollHeight;
+});
+
 function sendMessage() {
-    const messageInput = document.getElementById('message-input');
-    const message = messageInput.value;
-  
-    if (message.trim() !== '') {
-      const chatWindow = document.getElementById('chat-window');
-      const newMessage = document.createElement('p');
-      newMessage.textContent = message;
-      chatWindow.appendChild(newMessage);
-  
-      // Clear the input field after sending a message
-      messageInput.value = '';
-  
-      // Scroll to the bottom to show the latest message
-      chatWindow.scrollTop = chatWindow.scrollHeight;
-    }
+  const messageInput = document.getElementById('message-input');
+  const message = messageInput.value;
+
+  if (message.trim() !== '') {
+    // Emit the message to the server
+    socket.emit('message', message);
+
+    // Clear the input field after sending a message
+    messageInput.value = '';
   }
-  
-  
+}
